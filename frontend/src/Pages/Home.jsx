71fd5bf6d6d74fc04e6ecf66f570741f81133523
kpeset <kevin.peset@gmail.com>
2022-07-20 17:03:16 +0200

    creation et animation register

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef} from "react";
+import React, { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 
 import homePic from "../assets/img_home.jpg";
@@ -6,52 +6,48 @@ import homePic from "../assets/img_home.jpg";
 import("../styles/home.css");
 
 function Home() {
+  const sloganRef = useRef(null);
+  const searchRef = useRef(null);
+  const pictureRef = useRef(null);
 
+  const tl = gsap.timeline();
 
-const sloganRef = useRef(null)
-const searchRef = useRef(null)
-const pictureRef = useRef(null)
-
-const tl = gsap.timeline();
-
-useEffect(() => {
-  tl.from(pictureRef.current, {duration:2, x:-2000})
-  tl.to(pictureRef.current, {duration:0, x:1})
-  tl.from(sloganRef.current, {duration:2, x:1500, opacity: 0})
-  tl.to(sloganRef.current, {duration:0, x:1, opacity:1})
-  tl.from(searchRef.current, {duration:1, y:100, opacity: 0})
-  tl.to(searchRef.current, {duration:1, y:1, opacity: 1})
-
-
-
-
-
-}, [])
-
-
-
+  useEffect(() => {
+    tl.from(pictureRef.current, { duration: 2, x: -2000 });
+    tl.to(pictureRef.current, { duration: 0, x: 1 });
+    tl.from(sloganRef.current, { duration: 2, x: 1500, opacity: 0 });
+    tl.to(sloganRef.current, { duration: 0, x: 1, opacity: 1 });
+    tl.from(searchRef.current, { duration: 1, y: 100, opacity: 0 });
+    tl.to(searchRef.current, { duration: 1, y: 1, opacity: 1 });
+  }, []);
 
   return (
     <div
-    ref={pictureRef}
-    className="home-content"
-    style={{
-      backgroundImage: `url(${homePic})`,
-      backgroundSize: "110vh",
-      backgroundRepeat: "no-repeat",
-      backgroundPosition: "right",
-    }}
-  >
-    <div className="home-second-part">
-    <div className="home-slogan" ref={sloganRef}>
-      <h1>Echangez Avec Les <span className="bookipotes">Bookipotes</span> Vos<br /> Livres Déjà Lus</h1>
+      ref={pictureRef}
+      className="home-content"
+      style={{
+        backgroundImage: `url(${homePic})`,
+        backgroundSize: "110vh",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "right",
+      }}
+    >
+      <div className="home-second-part">
+        <div className="home-slogan" ref={sloganRef}>
+          <h1>
+            Echangez Avec Les <span className="bookipotes">Bookipotes</span> Vos
+            <br /> Livres Déjà Lus
+          </h1>
+        </div>
+        <div className="home-search" ref={searchRef}>
+          <input
+            type="text"
+            placeholder="Rechercher un livre ou un auteur..."
+          />
+          <button type="button">Chercher</button>
+        </div>
+      </div>
     </div>
-    <div className="home-search" ref={searchRef}>
-      <input type="text" placeholder="Rechercher un livre ou un auteur..." />
-      <button type="button">Chercher</button>
-   </div>
-   </div>
-  </div>
   );
 }
 
--- frontend/src/Pages/Register.jsx
+++ frontend/src/Pages/Register.jsx
@@ -1,9 +1,90 @@
-import ("../styles/register.css")
+import React, { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import registerPic from "../assets/img_register.jpg";
+import("../styles/register.css");
 
 function Register() {
-    return (
-        <div className="register-content"><h1>Hello</h1></div>
-    )
+  const formRef = useRef(null);
+  const titleRef = useRef(null);
+  const textRef = useRef(null);
+  const firstFormRef = useRef(null);
+  const secondFormRef = useRef(null);
+  const thirdFormRef = useRef(null);
+  const fourthFormRef = useRef(null);
+  const btnFormRef = useRef(null);
+
+
+  const tl = gsap.timeline();
+
+
+  useEffect(() => {
+    tl.from(formRef.current, {duration:2, x:-2000, opacity:0})
+    tl.to(formRef.current, {duration:0, x:1, opacity:1})
+    tl.from(titleRef.current, { duration: 1, opacity: 0 });
+    tl.to(titleRef.current, { duration: 0, opacity: 1 });
+    tl.from(textRef.current, { duration: 1, opacity: 0 });
+    tl.to(textRef.current, { duration: 0, opacity: 1 });
+    tl.fromTo(firstFormRef.current, { x:-800}, {x:1, duration:1} );
+    tl.fromTo(secondFormRef.current, { x:-800}, {x:1, duration:1} );
+    tl.fromTo(thirdFormRef.current, { x:-800}, {x:1, duration:1} );
+    tl.fromTo(fourthFormRef.current, { x:-800}, {x:1, duration:1} );
+    tl.fromTo(btnFormRef.current, { opacity:0}, {opacity:1, duration:1} );
+   
+  
+  
+  
+  
+  }, [])
+
+  return (
+    <div
+      className="register-content" ref={formRef}
+      style={{
+        backgroundImage: `url(${registerPic})`,
+        backgroundSize: "110vh",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "right",
+      }}
+    >
+      <div className="register-second-part">
+        <div ref={titleRef}>
+        <h1>Nous Rejoindre</h1>
+        </div>
+        <div ref={textRef}>
+        <h2>
+          Enregistrez-vous et ayez accès à la plus grande communauté de partage
+          de livres !
+        </h2>
+        </div>
+       
+        <form className="signin-form">
+          <input type="text" name="pseudo" placeholder="Votre pseudo" ref={firstFormRef}/>
+          <br />
+
+          <input type="email" name="email" placeholder="Votre email" ref={secondFormRef}/>
+          <br />
+
+          <input
+            type="password"
+            name="password"
+            ref={thirdFormRef}
+            placeholder="Choisissez un mot de passe"
+          />
+          <br />
+          <input
+            type="password"
+            name="checkPassword"
+            ref={fourthFormRef}
+            placeholder="Vérifiez votre mot de passe"
+          />
+          <br />
+          <button className="signin-btn" type="submit" ref={btnFormRef}>
+            VALIDER{" "}
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Register;
